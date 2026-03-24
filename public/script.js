6765481694637673f077febd6eb7622064ec5df0
sue-prog <sue@climb.aero>
2026-03-24 17:03:35 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-console.log("Frontend is connected.");
+async function testFunction() {
+  const response = await fetch("/functions/oral-exam");
+  const data = await response.json();
+  console.log("Backend says:", data);
+}
+
+testFunction();
