f1dd155ee5c3e5cc09267c7b8181431bda6b5a7d
sue-prog <sue@climb.aero>
2026-03-24 17:08:13 -0400

    Update oral-exam.js

--- functions/oral-exam.js
+++ functions/oral-exam.js
@@ -1,6 +1,5 @@
 export async function onRequest(context) {
-  return new Response(
-    JSON.stringify({ message: "Function is working!" }),
-    { headers: { "Content-Type": "application/json" } }
-  );
+  return new Response(JSON.stringify({ message: "Function is working!" }), {
+  headers: { "Content-Type": "application/json" }
+});
 }
