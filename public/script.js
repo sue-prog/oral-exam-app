c67b49ff06ebf209bced9be40eb423353f5578e5
sue-prog <sue@climb.aero>
2026-03-25 07:49:56 -0400

    Update script.js
    changed /functions/oral-exam to /oral-exam

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 async function testFunction() {
-  const response = await fetch("/functions/oral-exam");
+  const response = await fetch("/oral-exam");
   const data = await response.json();
   console.log("Backend says:", data);
 }
